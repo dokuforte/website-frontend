adb43798c23349b573b3a3434b1b4d25be4eb613
Richard Gazdik <richard.gazdik+github@gmail.com>
2022-05-02 21:53:18 +0200

    feat: Upload page tweaks

---
+++ src/api/album.js
@@ -0,0 +1,95 @@
+import config from "../data/siteConfig"
+
+/**
+ * Create empty album
+ * Should be called when no albumid provided
+ *
+ * @returns		JSON album record
+ */
+const createAlbum = async () => {
+  let resp = null
+  const authData = JSON.parse(localStorage.getItem("auth")) || {}
+  if (authData.access_token) {
+    resp = await fetch(`${config.API_HOST}/mydata/createalbum`, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        Authorization: `Bearer ${authData.access_token}`,
+      },
+    })
+  }
+  return resp ? resp.json() : resp
+}
+
+/**
+ * Load album data
+ *
+ * @returns		JSON album record
+ */
+const getAlbum = async id => {
+  let resp = null
+  // console.log(`${config.API_HOST}/mydata/getalbum?albumid=${id}`)
+  const authData = JSON.parse(localStorage.getItem("auth")) || {}
+  if (authData.access_token) {
+    resp = await fetch(`${config.API_HOST}/mydata/getalbum?albumid=${id}`, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        Authorization: `Bearer ${authData.access_token}`,
+      },
+    })
+  }
+  return resp ? resp.json() : resp
+  /**/
+}
+
+/**
+ * Edit unsubmitted album data
+ *
+ * @param formData	Form data object
+ * @returns			JSON: Modified rows count, new upload_album record
+ */
+const editAlbum = async formData => {
+  let resp = null
+  const authData = JSON.parse(localStorage.getItem("auth")) || {}
+  if (authData.access_token) {
+    resp = await fetch(`${config.API_HOST}/mydata/editalbum`, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        Authorization: `Bearer ${authData.access_token}`,
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(formData),
+    })
+  }
+  return resp ? resp.json() : resp
+}
+
+/**
+ * Submit uploaded album to moderators
+ *
+ * @param albumID	Album ID
+ * @returns			JSON: Modified rows count, new upload_album record
+ */
+const submitAlbum = async albumId => {
+  let resp = null
+  const authData = JSON.parse(localStorage.getItem("auth")) || {}
+  if (authData.access_token) {
+    resp = await fetch(`${config.API_HOST}/mydata/submitalbum?albumid=${albumId}`, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        Authorization: `Bearer ${authData.access_token}`,
+      },
+    })
+  }
+  return resp ? resp.json() : resp
+}
+
+export default {
+  createAlbum,
+  getAlbum,
+  editAlbum,
+  submitAlbum,
+}
--- src/layouts/profile-upload/profile-upload-controller.js
+++ src/layouts/profile-upload/profile-upload-controller.js
@@ -1,500 +1,135 @@
-import config from "../../data/siteConfig";
-import { Controller } from "stimulus";
-import { trigger, lang, getLocale, getURLParams } from "../../js/utils";
-import { initUploader, findElementByClassName } from "../../js/multiupload";
-import { datepicker } from "../../js/datepicker";
+import { Controller } from "stimulus"
+import authAPI from "../../api/auth"
+import albumAPI from "../../api/album"
+import { trigger, comeBackAfterSignIn } from "../../js/utils"
 
-const axios = require("axios").default;
-// const datepicker = require("js-datepicker").default;
-
-
-
-/**
- * Checkbox evaluate
- * 
- * @param value	checkbox value string
- * @returns		[0|1] 
- */
-function checkboxEvaluate (v) {
-	switch (v) {
-		case "1":
-		case "yes":
-		case "on":
-			return 1;
-		default:
-			return 0;
-	};
-}
-
-
-
-
-/**
- * API call wrapper
- *
- * @param endpoint	Endpoint path without hostname
- * @param method	[GET|POST] TODO - Implement full REST
- * @param headers	Custom headers. Optional.
- * @param body		Request body for POST call. Optional.
- * @param timeout   Timeout, default 10s
- * @param retry		Retry count before throwing network error exception. Default 3 
- * @returns			result JSON
- * @throws          timeoutException
- */
-async function apiCall(endpoint, method, headers = {}, body = {}, timeout = 10000, retry = 3) {
-	const authData = JSON.parse(localStorage.getItem("auth")) || {}
-
-	if (authData.access_token) {
-		var aaxios = new axios.create();
-		aaxios.defaults.headers.post["Authorization"] = `Bearer ${authData.access_token}`;
-		switch (method) {
-			case "GET":
-				aaxios.defaults.headers.get["Authorization"] = `Bearer ${authData.access_token}`;
-				aaxios.defaults.headers.get += headers;
-				aaxios.get(`${config.API_HOST}/${endpoint}`)
-					.then((res) => {
-						return (res);
-					})
-					.catch((e) => {
-						throw (e);
-					});
-				break;
-			case "POST":
-				aaxios.defaults.headers.post["Authorization"] = `Bearer ${authData.access_token}`;
-				aaxios.defaults.headers.post += headers;
-				aaxios.post(`${config.API_HOST}/${endpoint}`, body)
-					.then((res) => {
-						return (res);
-					})
-					.catch((e) => {
-						throw (e);
-					});
-				break;
-			default:
-				throw new Error("Invalid request method");
-		};
-	} else throw new Error("Unauthorized call");
-}
-
-
-
-/***
- * Create empty album
- * Should be called when no albumid provided
- *
- * @returns		JSON album record
- */
-const createAlbum = async () => {
-	let resp = null
-	/** /
-		await apiCall ("/mydata/createalbum", "GET")
-		.then ((res) => {
-			console.log (JSON.stringify (res));
-			return (res);		
-		})
-		.catch ((e) => {
-			console.error (e);
-			throw (e);
-		})
-	/**/
-	const authData = JSON.parse(localStorage.getItem("auth")) || {}
-	if (authData.access_token) {
-		resp = await fetch(`${config.API_HOST}/mydata/createalbum`, {
-			method: "GET",
-			mode: "cors",
-			headers: {
-				Authorization: `Bearer ${authData.access_token}`,
-			},
-		})
-	}
-	return resp ? resp.json() : resp
-	/**/
-}
-
-
-
-/***
- * Load album data
- *
- * @returns		JSON album record
- */
-const getAlbum = async id => {
-	let resp = null
-	console.log(`${config.API_HOST}/mydata/getalbum?albumid=${id}`);
-	const authData = JSON.parse(localStorage.getItem("auth")) || {}
-	if (authData.access_token) {
-		resp = await fetch(`${config.API_HOST}/mydata/getalbum?albumid=${id}`, {
-			method: "GET",
-			mode: "cors",
-			headers: {
-				Authorization: `Bearer ${authData.access_token}`,
-			},
-		})
-	}
-	return resp ? resp.json() : resp
-	/**/
-}
-
-
-
-/***
- * Load album list
- *
- * @returns		JSON album record
- */
-const getAlbumList = async id => {
-	let resp = null
-	console.log(`${config.API_HOST}/mydata/getalbumlist`);
-	const authData = JSON.parse(localStorage.getItem("auth")) || {}
-	if (authData.access_token) {
-		resp = await fetch(`${config.API_HOST}/mydata/getalbumlist`, {
-			method: "GET",
-			mode: "cors",
-			headers: {
-				Authorization: `Bearer ${authData.access_token}`,
-			},
-		})
-	}
-	return resp ? resp.json() : resp
-	/**/
-}
-
-
-
-/***
- * Edit unsubmitted album data
- *
- * @param formData	Form data object
- * @returns			JSON: Modified rows count, new upload_album record
- */
-const editAlbum = async formData => {
-	let resp = null
-	const authData = JSON.parse(localStorage.getItem("auth")) || {}
-	/*
-		const jsonData = JSON.stringify(Object.fromEntries(formData))
-			.replace(/\\"/g, '"')
-			.replace(/:"{"/g, ':{"')
-			.replace(/"}"/g, '"}') */
-	if (authData.access_token) {
-		resp = await fetch(`${config.API_HOST}/mydata/editalbum`, {
-			method: "POST",
-			mode: "cors",
-			headers: {
-				Authorization: `Bearer ${authData.access_token}`,
-				"Content-Type": "application/json; charset=utf-8",
-			},
-			body: JSON.stringify(formData),
-		});
-	};
-	return resp ? resp.json() : resp;
-}
-
-
-
-/***
- * Submit uploaded album to moderators
- *
- * @param albumID	Album ID
- * @returns			JSON: Modified rows count, new upload_album record
- */
-const submitAlbum = async albumId => {
-	let resp = null
-	const authData = JSON.parse(localStorage.getItem("auth")) || {}
-	if (authData.access_token) {
-		resp = await fetch(`${config.API_HOST}/mydata/submitalbum?albumid=${albumId}`, {
-			method: "GET",
-			mode: "cors",
-			headers: {
-				Authorization: `Bearer ${authData.access_token}`,
-			},
-		})
-	}
-	return resp ? resp.json() : resp
-}
-
-
+const AUTOSAVE_DELAY = 10000
 
 export default class extends Controller {
-	static get targets() {
-		return [
-			"form",
-			"fileSelector",
-			"fileSelectorPreview",
-			"submitalbum",
-			"submitalbumbutton",
-			"savealbumbutton",
-			"albumid",
-			"albumtitle",
-			"title",
-			"description",
-			"hebrewtitle",
-			"hebrewdescription",
-			"originalphotos",
-			"tags",
-			"date",
-			"date_approx",
-			"location",
-			"uploadalbum",
-			"uploadlist",
-			"uploadalbumlist",
-			"saveindicator",
-			"loadindicator",
-			"thankyou",
-		];
-	}
+  static get targets() {
+    return [
+      "fileSelector",
+      "form",
+      "albumTitle",
+      "title",
+      "description",
+      "hebrewTitle",
+      "hebrewDescription",
+      "tags",
+      "date",
+      "dateApprox",
+      "location",
+      "thankyou",
+    ]
+  }
 
+  async connect() {
+    const userData = await authAPI.querySignedInUser()
+    if (!userData) {
+      comeBackAfterSignIn()
+    } else {
+      this.formDataJSON = ""
+      this.formTarget.submit = this.submit.bind(this)
 
+      trigger("loader:show", { id: "loaderUpload" })
+      this.albumId = await this.getAlbumId()
 
-	initUploaderComponent(value = {}, albumid = 0) {
-		const authData = JSON.parse(localStorage.getItem("auth")) || {}
+      const res = await albumAPI.getAlbum(this.albumId)
+      if (res && res.count > 0) {
+        this.fillFormWithAlbumData(res.data[0])
+      }
+    }
 
-		if (!this.initialized) {
-			/** /
-						var initJSON = {
-							baseElement: this,
-							idListName: "originalphotos",
-							fileSelectorName: "image_uploads",
-							fileListName: "selected-files",
-						}
-			/**/
-			var initJSON = {
-				idListElement: this.originalphotosTarget,
-				fileSelectorElement: this.fileSelectorTarget,
-				fileListElement: this.fileSelectorPreviewTarget,
-			}
-			/**/
-			var xaxios = new axios.create();
-			xaxios.defaults.headers.post["Authorization"] = `Bearer ${authData.access_token}`;
-			initUploader(initJSON, xaxios, value, albumid);
-			this.initialized = true;
-		};
-	}
+    this.startAutoSaving()
+  }
 
+  async getAlbumId() {
+    let id = localStorage.getItem("albumId")
+    if (!id) {
+      // Create a new album if id doesn't exist
+      const res = await albumAPI.createAlbum()
+      if (res.count > 0) {
+        id = res.data[0].albumid
+        localStorage.setItem("albumId", id)
+      }
+    }
+    return id
+  }
 
+  startAutoSaving() {
+    trigger("loader:hide", { id: "loaderUpload" })
+    this.autoSaveInterval = setInterval(this.submit.bind(this), AUTOSAVE_DELAY, null)
+  }
 
-	connect() {
-		console.log("### connect");
+  stopAutoSaving() {
+    clearInterval(this.autoSaveInterval)
+  }
 
-		this.onPopState();
-		this.formTarget.submit = this.submit.bind(this);
-	}
+  fillFormWithAlbumData(data) {
+    if (data.original_photos) {
+      this.fileSelectorTarget.addFiles(data.original_photos)
+    }
 
+    this.albumTitleTarget.value = data.album_title || ""
+    this.titleTarget.value = data.title || ""
+    this.descriptionTarget.value = data.description || ""
+    this.hebrewTitleTarget.value = data.hebrew_title || ""
+    this.hebrewDescriptionTarget.value = data.hebrew_description || ""
+    this.tagsTarget.selectizeControl.value = data.tags || ""
+    this.locationTarget.selectizeControl.value = data.addressline || ""
+    this.dateTarget.value = data.date || ""
+    this.dateApproxTarget.checked = data.approx
+  }
 
-	
-	onPopState() {
-		function pageReady (that) {
-			var saveDelay;
+  submit(e) {
+    if (e) e.preventDefault()
 
-			that.loadindicatorTarget.style.display = "none";
+    // UPDATE ALBUM DATA
+    const formData = {
+      albumid: this.albumId,
+      album_title: this.albumTitleTarget.value,
+      title: this.titleTarget.value,
+      description: this.descriptionTarget.value,
+      hebrewtitle: this.hebrewTitleTarget.value,
+      hebrewdescription: this.hebrewDescriptionTarget.value,
+      tags: this.tagsTarget.selectizeControl.value.join(", "),
+      addressline: this.locationTarget.selectizeControl.value.join(", "),
+      date: this.dateTarget.value.length < 6 ? "null" : this.dateTarget.value,
+      approx: this.dateApproxTarget.checked,
+      original_photos: this.fileSelectorTarget.getUploadedFiles(),
+    }
 
-			var formInputs = [
-				that.albumtitleTarget,
-				that.titleTarget,
-				that.descriptionTarget,
-				that.hebrewtitleTarget,
-				that.hebrewdescriptionTarget,
-				that.originalphotosTarget,
-				that.tagsTarget,
-				that.dateTarget,
-				that.date_approxTarget,
-			 	that.locationTarget,
-			];
+    const formDataJSON = JSON.stringify(formData)
 
-			formInputs.forEach ((item) => {
-				item.addEventListener('change', resetSaveDelay);
-				item.addEventListener('keyup', resetSaveDelay);
-			});
+    // Submit data only if there's difference compared to the last save
+    if (this.formDataJSON !== formDataJSON) {
+      this.formDataJSON = formDataJSON
 
-			function startSaveDelay () {
-				saveDelay = setTimeout (saveAll, 5000);
-			}
+      trigger("loader:show", { id: "loaderUpload" })
+      albumAPI
+        .editAlbum(formData)
+        .then(() => {
+          trigger("loader:hide", { id: "loaderUpload" })
+        })
+        .catch(err => {
+          this.errorMessageHandler(err)
+          trigger("loader:hide", { id: "loaderUpload" })
+        })
+    }
+  }
 
-			function resetSaveDelay () {
-				try { clearTimeout (saveDelay); } catch (e) { ; };
-				startSaveDelay ();
-			}
+  async markAlbumForReview(e) {
+    if (e) e.preventDefault()
 
-			function saveAll () {
-				// alert ("Changed");
-				clearTimeout (saveDelay);
-				// that.savealbumbuttonTarget.click();
-				that.submit (null);
-			}
-		}
+    this.stopAutoSaving()
 
-		const fillData = (res) => {
-			const datePicker = datepicker (this.dateTarget, {
-				formatter: (input, date, instance) => {
-    				const value = date.toLocaleDateString ("en-GB");
-    				input.value = value; // => '1/1/2099';
-				}
-			});
-			console.log(JSON.stringify(res));
+    trigger("loader:show", { id: "loaderUpload" })
+    await albumAPI.submitAlbum(this.albumId)
+    trigger("loader:hide", { id: "loaderUpload" })
 
-			if (res.count > 0) {
-				console.log("### albumid", res.data[0].albumid);
+    this.formTarget.add("is-hidden")
+    this.thankyouTarget.remove("is-hidden")
 
-				this.albumidTarget.value = res.data[0].albumid;
-				this.albumtitleTarget.value = res.data[0].album_title || "";
-				this.titleTarget.value = res.data[0].title || "";
-				this.descriptionTarget.value = res.data[0].description || "";
-				this.hebrewtitleTarget.value = res.data[0].hebrew_title || "";
-				this.hebrewdescriptionTarget.value = res.data[0].hebrew_description || "";
-				this.originalphotosTarget.value = JSON.stringify(res.data[0].original_photos) || "";
-				this.tagsTarget.value = res.data[0].tags || "";
-				this.locationTarget.value = res.data[0].addressline || "";
-				this.dateTarget.value = res.data[0].date || "";
-				this.date_approxTarget.checked = res.data[0].approx ? 1 : 0;
-				
-				this.initUploaderComponent(res.data[0].original_photos, res.data[0].albumid);
-			};
-		};
-
-		var id = getURLParams().id;
-		switch (id) {
-			case (id > 0 ? id : 0):
-				lang ("upload")
-				.then ((txt) => {
-					var pageTitle = findElementByClassName (this.uploadlistTarget, "profile-content__title");
-					pageTitle.innerHTML = txt + " (" + id.toString () + ")"; 				
-				})
-				.catch ((e) => {});
-				
-				getAlbum(id)
-					.then((res) => {
-						this.uploadalbumTarget.style.display = "block";
-						this.uploadlistTarget.style.display = "none";
-						fillData(res);
-						pageReady(this);
-					});
-				break;
-			case "myalbums":
-				lang ("my_uploads")
-				.then ((txt) => {
-					var pageTitle = findElementByClassName (this.uploadlistTarget, "profile-content__title");
-					pageTitle.innerHTML = txt; 				
-				})
-				.catch ((e) => {});
-				
-				this.uploadlistTarget.style.display = "block";
-				this.uploadalbumTarget.style.display = "none";
-				
-				{
-					const listElement = document.createElement ("li");
-					listElement.className = "profile-upload__album-list";
-					const listLink = document.createElement ("a");
-					listLink.href = "/en/profile/upload?id=add";
-					lang ("new_topic").then ((res) => {	listLink.text = "New"; });
-					listElement.appendChild (listLink);
-					this.uploadlistTarget.appendChild (listElement);
-				}
-
-				getAlbumList ()
-				.then ((res) => {
-					res.data.forEach (n => {findElementByClassName
-						console.log (JSON.stringify (n));
-						const listElement = document.createElement ("li");
-						listElement.className = "profile-upload__album-list";
-						const listLink = document.createElement ("a");
-						listLink.href = "/en/profile/upload?id=" + n.albumid;
-						// listLink.className = "profile-upload__album-list";
-						listLink.text = n.date + " - " + n.album_title;
-						listElement.appendChild (listLink);
-						this.uploadlistTarget.appendChild (listElement);
-					// };
-					});
-					pageReady(this);
-				})
-				break;
-			case "new":
-			case "add":
-			default:
-				createAlbum()
-				.then((res) => {
-					this.uploadalbumTarget.style.display = "block";
-					this.uploadlistTarget.style.display = "none";
-					fillData(res);	
-					lang ("upload")
-					.then ((txt) => {
-						var pageTitle = findElementByClassName (this.uploadlistTarget, "profile-content__title");
-						pageTitle.innerHTML = txt + " (" + res.data[0].albumid.toString () + ")"; 				
-				})
-				.catch ((e) => {});
-					pageReady(this);
-				});
-				break;
-		};
-	}
-
-
-	async finish(e) {
-		if (e) e.preventDefault();
-
-		this.saveindicatorTarget.style.display = "block";
-		submitAlbum (this.albumidTarget.value)
-		.then ((res) => {
-			console.log (res);
-			this.saveindicatorTarget.style.display = "none";
-			this.uploadalbumTarget.style.display = "none";
-			this.thankyouTarget.style.display = "block";
-		});
-	}
-
-
-	async submit(e) {
-		if (e) e.preventDefault();
-
-		this.saveindicatorTarget.style.display = "block";
-
-		// UPDATE ALBUM DATA
-		var formData = {};
-		formData["albumid"] = this.albumidTarget.value;
-		formData["album_title"] = this.albumtitleTarget.value;
-		formData["title"] = this.titleTarget.value;
-		formData["description"] = this.descriptionTarget.value;
-		formData["hebrewtitle"] = this.hebrewtitleTarget.value;
-		formData["hebrewdescription"] = this.hebrewdescriptionTarget.value;
-		formData["tags"] = this.tagsTarget.selectizeControl.value.join(", ");
-		formData["addressline"] = this.locationTarget.selectizeControl.value.join(", ");
-		formData["date"] = this.dateTarget.value.length < 6 ? "null" : this.dateTarget.value;
-		formData["approx"] = checkboxEvaluate (this.date_approxTarget.value);
-		formData["original_photos"] = JSON.parse(this.originalphotosTarget.value);
-		/**/
-
-		console.log("### formdata - ", JSON.stringify(formData));
-
-		editAlbum(formData)
-			.then(resp => {
-				console.log(resp)
-				this.saveindicatorTarget.style.display = "none";
-			})
-			.catch(err => {
-				this.errorMessageHandler(err)
-				this.saveindicatorTarget.style.display = "none";
-			});
-	}
-
-	// the server response returns a string based error message in English
-	// so it needs to be localized
-	async errorMessageHandler(text) {
-		console.error("errorHandler: ", text);
-
-		const errorMessages = {
-			"The user has not been activated or is blocked.": await lang("user_signin_error"),
-			"Invalid user credentials.": await lang("user_signin_error"),
-			'"email" must be a valid email': await lang("user_signin_error"),
-			'"email" is not allowed to be empty': await lang("user_signin_error"),
-		}
-
-		return errorMessages[text] || text
-	}
-
-
-
-	chooseFiles(e) {
-		e.preventDefault();
-		this.initUploaderComponent();
-		this.fileSelectorTarget.click();
-	}
-
+    localStorage.removeItem("albumId")
+  }
 }
