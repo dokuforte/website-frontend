cff648d3cb7970b104155cfa8fa5e0bd35939db2
Richard Gazdik <richard.gazdik+github@gmail.com>
2024-02-14 21:57:56 +0100

    fix: t.contains is not supported

--- src/js/utils.js
+++ src/js/utils.js
@@ -278,7 +278,7 @@ export const formDataToJson = (formData) => {
 
 export const getApiUrl = () => {
   const { protocol, hostname } = window.location
-  const slice = hostname.contains(".com") || hostname.contains(".org") ? -2 : -3
+  const slice = hostname.indexOf(".com") !== -1 || hostname.indexOf(".org") !== -1 ? -2 : -3
   const domain = hostname.split(".").slice(slice).join(".")
   return `${protocol}//backend.${domain}`
 }
