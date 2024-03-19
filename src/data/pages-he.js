7bf5d5ce78997f96da5435ab8c120159a3f46111
Richard Gazdik <richard.gazdik+github@gmail.com>
2024-03-19 23:07:59 +0100

    fix: generate pages based on CMS content

---
+++ src/data/pages-he.js
@@ -0,0 +1,13 @@
+const EleventyFetch = require("@11ty/eleventy-fetch")
+const config = require("./siteConfig")
+
+module.exports = async function () {
+  console.log("Fetching pages[HE] data...")
+  const url = `${config.API_HOST}/api/contents?lang=he`
+
+  /* This returns a promise */
+  return EleventyFetch(url, {
+    duration: "1d", // save for 1 minute
+    type: "json", // we’ll parse JSON for you
+  })
+}
